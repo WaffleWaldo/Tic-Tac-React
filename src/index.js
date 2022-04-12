dc919198e701414c241ceb5446a9c337ca6d1d7c
WaffleWaldo <oswaldoortiz0519@gmail.com>
2022-04-12 12:26:41 -0400

    split return element into multiple lines

--- src/index.js
+++ src/index.js
@@ -32,10 +32,12 @@ class Square extends React.Component {
       }
 
     renderSquare(i) {
-      return <Square
-                 value={this.state.squares[i]}
-                onClick={() => this.handleClick(i)}
-            />;
+      return (
+        <Square 
+            value={this.state.squares[i]} 
+            onClick={() => this.handleClick(i)}
+        />
+      );
     }
   
     render() {
